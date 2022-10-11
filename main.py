a88b9f0949fb3f55ca8a70d3c960daaee054da65
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 01:29:29 +0000

    7-4

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 numbers = [5, 20, 30, 30, 50]
 delval = int(input('Enter the deletion value: '))
-for i in range(len(numbers)):
-    if numbers[i] == delval:
-        numbers.remove(numbers[i])
+while True:
+    try:
+        numbers.remove(delval)
+    except:
+        break
 
 # ******************************
 # Make your Code
